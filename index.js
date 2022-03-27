90d0b31fd64ae9169df2afb7abf11257db2cf541
Muhammed batuhan cakir <bthn.ckr.24@gmail.com>
2022-03-27 18:31:45 +0300

    master1

--- index.js
+++ index.js
@@ -6,7 +6,28 @@
 //     console.log("merhaba dünya")
 // }, 2000);
 
-import fetch from "node fetch" 
+import fetch from "node-fetch"
 
-fetch("https://jsonplaceholder.typicode.com/posts").then((data) => data.json()).then(users => console.log(users)
-)
+// fetch("https://jsonplaceholder.typicode.com/posts").then((data) => data.json()).then(users => console.log(users)
+// )
+
+// async function getData(){
+//     const data = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     console.log(data)
+// }
+// getData()
+
+
+//  const users = [
+//             {
+//                 name:"mehmet",
+//             },
+//             {
+//                 name:"ali",
+//             },
+//             {
+//                 name:"batu",
+//             }];
+
+//  const result = users.find((item) => item.name === "batu")
+//  console.log(result)
